e5147259ce880ca5f730ae90dc193ca6bb024992
Justin Dela Cruz <justinavdc@gmail.com>
2026-07-02 00:32:26 -0700

    Special offers added

--- Scripts/home_page.js
+++ Scripts/home_page.js
@@ -1,10 +1,8 @@
-const bookButton = document.getElementById("book-button")
-
-function bookHoverBehavior() {
-    bookButton.style.backgroundColor = "rgb(20, 24, 50)";
+function bookHoverBehavior(element) {
+    element.style.backgroundColor = "rgb(20, 24, 50)";
 }
 
-function bookIdleBehavior() {
-    bookButton.style.backgroundColor = "rgb(10, 22, 98)";
-    bookButton.style.color = "rgb(239, 214, 52)";
+function bookIdleBehavior(element) {
+    element.style.backgroundColor = "rgb(10, 22, 98)";
+    element.style.color = "rgb(239, 214, 52)";
 }
--- Scripts/site.js
+++ Scripts/site.js
@@ -16,12 +16,16 @@ function headerIdleBehavior(element) {
 function slideshowHoverBehavior(slideshow, button1, button2) {
     var b1 = document.getElementById(button1);
     var b2 = document.getElementById(button2);
+    b1.style.backgroundColor = "rgba(51, 60, 122, 0.5)";
+    b2.style.backgroundColor= "rgba(51, 60, 122, 0.5)";
     slideshow.style.boxShadow = "0mm 0mm 10mm black";
 }
 
 function slideshowIdleBehavior(slideshow, button1, button2) {
     var b1 = document.getElementById(button1);
     var b2 = document.getElementById(button2);
+    b1.style.backgroundColor = "rgba(20, 24, 50, 0.5)";
+    b2.style.backgroundColor= "rgba(20, 24, 50, 0.5)";
     slideshow.style.boxShadow = "0mm 0mm 2mm black";
 }
 
