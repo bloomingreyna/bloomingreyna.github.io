983062ec75dee1789e83332f7084fb203040e688
Justin Dela Cruz <justinavdc@gmail.com>
2026-07-08 00:06:08 -0700

    revamp progress

---
+++ .refactor/Home/script.js
@@ -0,0 +1,16 @@
+const introTextboxes = document.getElementById("intro-textboxes");
+const altCallToAction = document.getElementById("call-to-action-row");
+const introImage = document.getElementById("intro-image");
+
+const aspectRatioQuery = window.matchMedia("(max-aspect-ratio: 3/2)");
+
+window.addEventListener("resize", resizeIntroImage);
+window.addEventListener("load", resizeIntroImage);
+
+function resizeIntroImage() {
+    if (!aspectRatioQuery.matches) {
+        introImage.style.height = `${introTextboxes.getBoundingClientRect().height}px`;
+    } else {
+        introImage.style.height = `${altCallToAction.getBoundingClientRect().height}px`;
+    }
+}
