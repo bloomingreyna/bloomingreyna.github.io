10a91de1fd186e10b86bae4f27018cd208706f66
Justin Dela Cruz <justinavdc@gmail.com>
2026-07-09 18:07:46 -0700

    otsa agua

--- refactor/General/Scripts/custom_elements.js
+++ refactor/General/Scripts/custom_elements.js
@@ -17,14 +17,42 @@ class SiteHeader extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <header class="site-header">
+            <header id="site-header" class="site-header">
                 <h1 class="site-header" style="margin-right: 1rem;"><a href="https://www.weldiscleaning.com">WELDI'S CLEANING</a></h1>
                 <h2 class="site-header" style="color: ${this.checkPage(this.homePath) ? "white" : "auto"};"><a href="../Home/page.html">HOME</a></h2>
                 <h2 class="site-header" style="color: ${this.checkPage(this.servicesPath) ? "white" : "auto"};"><a href="../Services/page.html">SERVICES</a></h2>
                 <h2 class="site-header" style="color: ${this.checkPage(this.serviceAreasPath) ? "white" : "auto"};">SERVICE AREAS</h2>
                 <h2 class="site-header" style="color: ${this.checkPage(this.bookOnlinePath) ? "white" : "auto"};">BOOK ONLINE</h2>
+                <img class="site-header" src="../General/Images/chevchev.webp" alt="Menu dropdown">
             </header>
         `;
     }
 }
 customElements.define("site-header", SiteHeader);
+
+class SiteFooter extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                <div class="column-container" style="gap: 5px;">
+                    <h1>SITE PAGES</h1>
+                    <p><a href="">Home</a></p>
+                    <p><a href="">Services</a></p>
+                    <p><a href="">Service Areas</a></p>
+                    <p><a href="">Book Online</a></p>
+                </div>
+
+                <div class="column-container" style="gap: 5px;">
+                    <h1>CONTACT US</h1>
+                    <p><b>Phone: </b>(408) 621-9929</p>
+                    <p><b>Email: </b>weldiscleaning@gmail.com</p>
+                </div>
+            </footer>
+        `;
+    }
+}
+customElements.define("site-footer", SiteFooter);
--- refactor/Services/script.js
+++ refactor/Services/script.js
@@ -1,8 +1,16 @@
 const serviceContent = document.getElementsByClassName("service-content");
+const serviceTabButtons = document.getElementsByClassName("service-tab-button");
 
-function switchToServiceTab(index) {
+function switchToServiceTab(index, button) {
     for (var i = 0; i < serviceContent.length; i++) {
         serviceContent[i].style.display = "none";
+        
     }
+
+    for (var i = 0; i < serviceTabButtons.length; i++) {
+        serviceTabButtons[i].style.color = "white";
+    }
+
     serviceContent[index].style.display = "block";
+    button.style.color = "rgb(239, 214, 52)";
 }
